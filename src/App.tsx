c53413460923a4a218478826c87ee029e31f3dce
ternyavsky <ilya.ternyavsky@yandex.ru>
2025-05-31 17:42:50 +0300

    add create order

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/auth-context";
 import { Layout } from "./components/Layout";
 import { TanstackContext } from "./context/tanstack-context";
